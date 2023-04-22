cc5402a4e9c299f53419f536480d3f6b9a2a452a
pangqiang <pangqiang@users.noreply.github.com>
2023-04-22 09:10:57 +0000

    deploy: 55034edcf066ff81bc4a282ee33e6ac45c9bb53a

--- js/darkLightToggle.js
+++ js/darkLightToggle.js
@@ -1,19 +1,17 @@
 (function(){
-    if(localStorage.getItem('theme-mode')){
-        document.getElementsByTagName("html")[0].id=localStorage.getItem('theme-mode');
+    const theme = localStorage.getItem('theme-mode');
+    if(theme){
+        document.getElementsByTagName("html")[0].classList.add(theme);
     }
 }())
 
 function darkLightToggle() {
-    
-    if (document.getElementById("theme-light-mode") != null) {
-        document.getElementById("theme-light-mode").id="theme-dark-mode";
-        localStorage.setItem('theme-mode','theme-dark-mode')
-        return;
-    }
-    if (document.getElementById("theme-dark-mode") != null) {
-        document.getElementById("theme-dark-mode").id="theme-light-mode";
-        localStorage.setItem('theme-mode','theme-light-mode')
-        return;
+    const _html = document.getElementsByTagName("html")[0];
+    if(_html.classList.contains('dark')){
+        _html.classList.remove('dark')
+        localStorage.setItem('theme-mode','light')
+    }else{
+        _html.classList.add('dark')
+        localStorage.setItem('theme-mode','dark')
     }
 }
